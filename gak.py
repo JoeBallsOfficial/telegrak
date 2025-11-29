482cc9fb19fc79d671f23ca455455689d4d654f6
zogog <kokikuk@outlook.com>
2025-11-29 23:28:22 +0300

    api test

--- gak.py
+++ gak.py
@@ -1,2 +1,10 @@
-print("gak")
-print('Bakak')
+import requests as rq
+
+api_url = 'http://api.open-notify.org/iss-now.json'
+
+response = rq.get(api_url)
+
+if response.status_code == 200:
+    print(response.text)
+else:
+    print(response.status_code)
