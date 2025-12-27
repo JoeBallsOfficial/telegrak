08696d39babeb61287b7f0bcec0a219ae584b892
zogog <kokikuk@outlook.com>
2025-12-28 00:29:22 +0300

    added cats

--- gak.py
+++ gak.py
@@ -5,21 +5,31 @@ with open("token.txt", "r") as file:
     BOT_TOKEN = file.readline().strip()
 
 API_URL = 'https://api.telegram.org/bot'
+CAT_API_URL = "https://api.thecatapi.com/v1/images/search"
 TEXT = "Вижу"
-MAX_COUNTER = 100
+ERROR_TEXT = "неработет"
+MAX_COUNTER = 15
 
 offset = -2
 counter = 0
 chat_id: int
+cat_link: str
+cat_response: rq.Response
 
 while counter < MAX_COUNTER:
+    print("attempt=", counter)
     update = rq.get(f"{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}").json()
 
     if update["result"]:
         for result in update["result"]:
             offset = result["update_id"]
             chat_id = result["message"]["from"]["id"]
-            rq.get(f"{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}")
-    
+            cat_response = rq.get(CAT_API_URL)
+            if cat_response.status_code == 200:
+                cat_link = cat_response.json()[0]["url"]
+                rq.get(f"{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}")
+            else:
+                rq.get(f"{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}")
+                
     time.sleep(1)
     counter += 1
