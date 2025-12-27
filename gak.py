b02b43dc140dc99225e30be0f82ff1381c4912ff
zogog <kokikuk@outlook.com>
2025-12-27 14:55:59 +0300

    apitest

--- gak.py
+++ gak.py
@@ -1,12 +1,25 @@
 import requests as rq
+import time
 
-api_url = 'http://api.open-notify.org/iss-now.json'
+with open("token.txt", "r") as file:
+    BOT_TOKEN = file.readline().strip()
 
-response = rq.get(api_url)
+API_URL = 'https://api.telegram.org/bot'
+TEXT = "Вижу"
+MAX_COUNTER = 100
 
-if response.status_code == 200:
-    print(response.text)
-else:
-    print(response.status_code)
+offset = -2
+counter = 0
+chat_id: int
 
-print('bagagwa')
+while counter < MAX_COUNTER:
+    update = rq.get(f"{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}").json()
+
+    if update["result"]:
+        for result in update["result"]:
+            offset = result["update_id"]
+            chat_id = result["message"]["from"]["id"]
+            rq.get(f"{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}")
+    
+    time.sleep(1)
+    counter += 1
